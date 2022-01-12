e946a1875a4382c73038efbe0a96793177b59b77
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-01-13 02:19:54 +0900

    add user method on Article model

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,47 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = [
-            (object) [
-                'id' => 1,
-                // 'title' => 'タイトル1',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque laborum quasi repellendus dicta iste voluptas nesciunt, autem quas placeat velit. Dicta voluptas cum, rerum magnam minima repellendus neque voluptatem voluptatibus.',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                    'at_name' => 'hello_my_titter',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                // 'title' => 'タイトル2',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque laborum quasi repellendus dicta iste voluptas nesciunt, autem quas placeat velit. Dicta voluptas cum, rerum magnam minima repellendus neque voluptatem voluptatibus.',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名2',
-                    'at_name' => 'hello_my_titter2',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                // 'title' => 'タイトル3',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque laborum quasi repellendus dicta iste voluptas nesciunt, autem quas placeat velit. Dicta voluptas cum, rerum magnam minima repellendus neque voluptatem voluptatibus.',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名3',
-                    'at_name' => 'hello_my_titter',
-                ],
-            ],
-        ];
+        // $articles = [
+        //     (object) [
+        //         'id' => 1,
+        //         // 'title' => 'タイトル1',
+        //         'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque laborum quasi repellendus dicta iste voluptas nesciunt, autem quas placeat velit. Dicta voluptas cum, rerum magnam minima repellendus neque voluptatem voluptatibus.',
+        //         'created_at' => now(),
+        //         'user' => (object) [
+        //             'id' => 1,
+        //             'name' => 'ユーザー名1',
+        //             'at_name' => 'hello_my_titter',
+        //         ],
+        //     ],
+        //     (object) [
+        //         'id' => 2,
+        //         // 'title' => 'タイトル2',
+        //         'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque laborum quasi repellendus dicta iste voluptas nesciunt, autem quas placeat velit. Dicta voluptas cum, rerum magnam minima repellendus neque voluptatem voluptatibus.',
+        //         'created_at' => now(),
+        //         'user' => (object) [
+        //             'id' => 2,
+        //             'name' => 'ユーザー名2',
+        //             'at_name' => 'hello_my_titter2',
+        //         ],
+        //     ],
+        //     (object) [
+        //         'id' => 3,
+        //         // 'title' => 'タイトル3',
+        //         'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque laborum quasi repellendus dicta iste voluptas nesciunt, autem quas placeat velit. Dicta voluptas cum, rerum magnam minima repellendus neque voluptatem voluptatibus.',
+        //         'created_at' => now(),
+        //         'user' => (object) [
+        //             'id' => 3,
+        //             'name' => 'ユーザー名3',
+        //             'at_name' => 'hello_my_titter',
+        //         ],
+        //     ],
+        // ];
+
+        $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', compact('articles'));
     }
